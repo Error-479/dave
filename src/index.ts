ee77636bbc385b7c839b5493d176b9b8b90891d5
Courage <72545275+piratekingdev@users.noreply.github.com>
2021-10-06 17:37:02 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import { Client, Intents } from 'discord.js';
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ 
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+ });
 
 const prefix = '!';
 
-client.on('ready', () => {
+client.once('ready', () => {
   console.log(`Logged in as ${client.user?.tag}!`);
 });
 
-client.on('message', m => {
+client.on('messageReactionAdd', (reaction, user) => {
+  if (reaction.message.id === '895372392845688874') {
+    if (reaction.emoji.name === '🍎'){
+      console.log('Apple emoji detected for specific message!');
+      /* TODO: Check if the bot has the manage_messages permission before
+         trying to remove a reaction to avoid potential errors */
+      reaction.remove();
+    }
+  }
+});
+
+client.on('messageCreate', m => {
   if (m.author.bot) return;
   if (m.content[0] === prefix) {
     const args = m.content.slice(prefix.length).split(/(\s+)/);
