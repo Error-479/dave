13b161ba24d6426e6bebebbc56de8c1e665ee14e
Courage <72545275+piratekingdev@users.noreply.github.com>
2021-10-23 12:40:29 -0500

    First attempt at issue #12 (#14)
    * Update README.md
    
    * Update README.md
    
    * Initial prettierrc
    
    * Delete .prettierrc
    
    * Initial prettier configuration
    
    This initial prettier configuration will force the following style guidelines:
    
    * A default tab width of 4
    * Enforce the use of tabs
    * A maximum of 120 characters per line
    * Enforce single quotes (ex: 'hello world')
    * Enforce bracket spacing (ex: { 'example' })
    * \n line endings. This will help resolve any formatting issues between Windows and Unix-like operating systems
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update .prettierrc
    
    Removed options that were using default settings.
    
    * Update .prettierrc
    
    * Create .prettierrc
    
    * Update index.ts
    
    * Update index.ts
    
    * Update index.ts
    
    Fixed formatting issues
    
    * Update index.ts
    
    Fixed formatting?
    
    * Delete index.ts
    
    * Create index.ts
    
    * Fixed code formatting
    
    * Code formatting
    
    * Update index.ts
    
    * Update index.ts
    
    * Fixed formatting issues
    
    * Update index.ts
    
    * Converted all spaces to tabs
    
    * Converted all spaces to tabs
    
    * Changed client.once to client.on

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,49 @@
 import { Client, Intents } from 'discord.js';
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
+	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+});
 
 const prefix = '!';
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user?.tag}!`);
+	console.log(`Logged in as ${client.user?.tag}!`);
 });
 
-client.on('message', m => {
-  if (m.author.bot) return;
-  if (m.content[0] === prefix) {
-    const args = m.content.slice(prefix.length).split(/(\s+)/);
-    console.log(args);
-    if (args[0] === 'help') {
-      m.reply('no');
-    }
-  }
+client.on('messageReactionAdd', async (reaction, user) => {
+	// Cache message
+	if (reaction.partial) {
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Failed to fetch: ', error);
+			return;
+		}
+	}
+
+	// Process reactions
+	if (reaction.message.content != null && reaction.message.content.includes(prefix + 'reacttest')) {
+		if (reaction.emoji.name === '🍎') {
+			console.log('Someone reacted with an apple emoji in a message that contained the text !reacttest.');
+			try {
+				await reaction.remove();
+			} catch (error) {
+				console.log('Failed to remove reaction: ', error);
+				return;
+			}
+		}
+	}
+});
+
+client.on('messageCreate', (m) => {
+	if (m.author.bot) return;
+	if (m.content[0] === prefix) {
+		const args = m.content.slice(prefix.length).split(/(\s+)/);
+		console.log(args);
+		if (args[0] === 'help') {
+			m.reply('no');
+		}
+	}
 });
 
 client.login(process.env.DISCORD_TOKEN);
